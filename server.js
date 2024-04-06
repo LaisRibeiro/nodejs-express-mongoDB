798ed7811cb03020a108cc8971a81221de66b79b
LaisRibeiro <riibeirolais@gmail.com>
2024-04-06 20:04:39 -0300

    Criando o CRUD

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
-import http from "http"
+import app from "./src/app.js";
 
 // Usar padrão de variável maiúscula quando passado informações fixas/estáticas
 const PORT = 3000;
 
-const rotas = {
-    "/": "Curso de Noje.js",
-    "/livros": "Entrei na rota Livros",
-    "/autores": "Entrei na rota Autores"
-};
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    //res.end = texto que vai ser trafegado
-    res.end(rotas[req.url]);
-});
+//-----Era utilizado pela biblioteca HTTP
+// const rotas = {
+//     "/": "Curso de Noje.js",
+//     "/livros": "Entrei na rota Livros",
+//     "/autores": "Entrei na rota Autores"
+// };
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, { "Content-Type": "text/plain" });
+//     //res.end = texto que vai ser trafegado
+//     res.end(rotas[req.url]);
+// });
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log("Servidor tá on!");
 });
